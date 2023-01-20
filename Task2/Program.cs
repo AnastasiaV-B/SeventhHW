c3278237f674d8633b448d6b7d9486c3600f8d31
garnova_n@mail.ru <garnova_n@mail.ru>
2023-01-20 13:27:00 +0300

    решено 2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -7,3 +7,47 @@
 // 5 9 2 3
 // 8 4 2 4
 // 17 -> такого числа в массиве нет
+
+int InputNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int n = InputNumber("введите номер строки");
+int m = InputNumber("введите номер столбца");
+
+int [,] numbers = new int [10,10];
+FillArrayRandomNumbers(numbers);
+
+if (n > numbers.GetLength(0) || m > numbers.GetLength(1))
+{
+    Console.WriteLine("такого числа в массиве нет");
+}
+else
+{
+    Console.WriteLine($"{n} & {m} == {numbers[n-1,m-1]}");
+}
+PrintArray(numbers);
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        {        
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array [i,j] = new Random().Next(-100, 100)/10;
+            }   
+        }
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + ",");
+        }   
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
